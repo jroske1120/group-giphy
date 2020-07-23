14c6dc640b2159e5aaebaf100981d3cfc4bed0d2
Ross Hutchens <rlhcxm@gmail.com>
2020-07-23 11:21:40 -0500

    installed redux-saga and dotenv

--- src/components/ImageSearch/ImageSearch.js
+++ src/components/ImageSearch/ImageSearch.js
@@ -4,10 +4,32 @@ import ImageDisplay from '../ImageDisplay/ImageDisplay';
 
 class ImageSearch extends Component {
 
+  state = {
+    search: ''
+  }
+
+  handleSubmit = (event) => {
+    event.preventDefault();
+    console.log('handleSubmit:', this.state.search);
+    // this.props.dispatch({type: "SET_IMAGE", payload: this.state.search})
+
+  }
+
+  handleChange = (event) => {
+    console.log('in search field', event.target.value);
+    this.setState({
+      search: event.target.value
+    })
+  }
+
   render() {
     return (
       <div>
         <h1>Giphy Search!</h1>
+        <form onSubmit={this.handleSubmit}>
+          <input type="text" onChange={this.handleChange} placeholder="Search"></input>
+          <button type="submit">Search</button>
+        </form>
         <ImageDisplay/>
       </div>
     );
